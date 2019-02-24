b8846e0c482394fdb71e2639071ebe2fa230f8fd
Daniel Ruf <daniel@daniel-ruf.de>
2019-02-24 22:08:07 +0100

    feature: use new hooks API for webpack 4 and newer

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ function FilterPlugin(options) {
     files = options.files
 }
 
-FilterPlugin.prototype.apply = function(compiler) {
-  compiler.plugin('emit', function(compilation, callback) {
-    if(typeof files !=='undefined' && files.length){
-        if(files.constructor === Array){
-            for(i = 0; i < files.length; i++){
+const cb = function (compilation, callback) {
+    if (typeof files !== 'undefined' && files.length) {
+        if (files.constructor === Array) {
+            for (i = 0; i < files.length; i++) {
                 delete compilation.assets[files[i]]
             }
         } else {
@@ -14,7 +13,14 @@ FilterPlugin.prototype.apply = function(compiler) {
         }
     }
     callback()
-  })
+}
+
+FilterPlugin.prototype.apply = function (compiler) {
+    if (compiler.hooks) {
+        compiler.hooks.emit.tapAsync('FilterPlugin', cb)
+    } else {
+        compiler.plugin('emit', cb)
+    }
 }
 
 module.exports = FilterPlugin
